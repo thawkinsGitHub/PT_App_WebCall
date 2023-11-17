b3e823575d40e1c725c65c9ca883f366ce44f67d
Tucker Hawkins <63420822+thawkinsGitHub@users.noreply.github.com>
2023-11-17 12:15:36 -0500

    Initial Commit

---
+++ PT_App_WebCall/PT_App_WebCallApp.swift
@@ -0,0 +1,17 @@
+//
+//  PT_App_WebCallApp.swift
+//  PT_App_WebCall
+//
+//  Created by Tucker Hawkins on 11/17/23.
+//
+
+import SwiftUI
+
+@main
+struct PT_App_WebCallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PT_App_WebCallUITests/PT_App_WebCallUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  PT_App_WebCallUITestsLaunchTests.swift
+//  PT_App_WebCallUITests
+//
+//  Created by Tucker Hawkins on 11/17/23.
+//
+
+import XCTest
+
+final class PT_App_WebCallUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
