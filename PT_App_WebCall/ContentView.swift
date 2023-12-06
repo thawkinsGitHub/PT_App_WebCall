e07df0795cb6ddeeb5fc963f733fb9e25dd85387
Tucker Hawkins <63420822+thawkinsGitHub@users.noreply.github.com>
2023-12-06 15:07:04 -0500

    Added stuff

--- PT_App_WebCall/ContentView.swift
+++ PT_App_WebCall/ContentView.swift
@@ -7,15 +7,38 @@
 
 import SwiftUI
 
+class WebCall {
+    let url = URL(string: "https://www.webservices.parktudorcs.org/api/TodoItems")!
+    func loadText(completion:@escaping (String) -> ()) {
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let data = data {
+                print(data)
+                let text1 = String(data: data, encoding: .utf8)
+                print(text1 ?? "Blank")
+                DispatchQueue.main.async {
+                    completion(text1 ?? "Loaded")
+                }
+            } else if let error = error {
+                print("HTTP Request Failed \(error)")
+            }
+        }
+            .resume()
+    }
+}
+
+
 struct ContentView: View {
+    @State var text1: String?
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(text1 ?? "Not loaded")
+        }.onAppear() {
+            WebCall().loadText { (text1) in
+                self.text1 = text1
+            }
         }
         .padding()
+        
     }
 }
 
